2ecf20e790bf18ca928f27b43bbf2d7bf253bad0
Vladexy88 <jv69986998@gmail.com>
2021-10-11 16:36:10 +0300

    Update HomeController.cs

--- WebApplicationMvc/Controllers/HomeController.cs
+++ WebApplicationMvc/Controllers/HomeController.cs
@@ -32,9 +32,9 @@ namespace WebApplicationMvc.Controllers
         public async Task<IActionResult> Tab()
         {
             var url = "https://tproger.ru/";
-            ParserHref final = new ParserHref();
-            await final.GetDataAsync(url);
-            var data = final.DataTable;
+            ParserHref parserHref = new ParserHref();
+            await parserHref.GetDataAsync(url);
+            var data = parserHref.DataTable;
             return View(data);
         }
 
